70a2dc564fe9529df55814f06a4e2e5ffed864ab
Kris Frey <kfrey@mit.edu>
2015-11-19 16:58:39 -0500

    Wrote process model

--- model.py
+++ model.py
@@ -35,7 +35,7 @@ init_mu[VAR_POS_Z]      = -0.05
 init_mu[VAR_GBIAS_P]    =  0
 init_mu[VAR_GBIAS_R]    =  0
 init_mu[VAR_GBIAS_Q]    =  0
-init_mu[VAR_THRUST]     = grav_acc
+init_mu[VAR_SP_THRUST]     = grav_acc
 init_mu[VAR_OF_FL]      = 1.0
 init_mu[VAR_DRAG_CO]    = 1.0
 
@@ -47,12 +47,17 @@ init_Sigmas[VAR_POS_X:VAR_POS_Z+1]  = 1e-3*np.ones(3)  # we are pretty certain b
 init_Sigmas[VAR_GBIAS_P:VAR_GBIAS_R+1] = 1e-2*np.ones(3)
 init_Sigmas[VAR_DRAG_CO]            = 2.0
 
+# Disturbance modes
+DISTURB_NOMINAL = 0   # no disturbance detected
+DISTURB_ACTIVE  = 1   # disturbance detected, inflate process
+
 ## Process Models
-def process_model(x, u, dt):
+def process_model(x, u, dt, disturb_mode):
 # Inputs:
 #       x:      Current vehicle state
 #       u:      Control signal (raw gyro rates)
 #       dt:     Propogation time [sec]
+#       disturb_mode:  Flag to use disturbance mode
 # Outputs:
 #       f:      Predicted state
 #       Fx:     Jacobian wrt to state
@@ -60,11 +65,125 @@ def process_model(x, u, dt):
 #       M:      Measurement covariance matrix rate
 #       R:      Process model covariance matrix rate
   
-  # calculate CT process model
-  A = np.zeros(VAR_COUNT, VAR_COUNT)
+    # extract useful state variables
+    vel_u = x[VAR_VEL_U]
+    vel_v = x[VAR_VEL_V]
+    vel_w = x[VAR_VEL_W]
+    thrust = x[VAR_SP_THRUST]
+    drag_coeff  = x[VAR_DRAG_CO]
+    gyro_biases = x[VAR_GBIAS_P:UAV_GBIAS_P+3]
 
-  return (f, Fx, Fu, M, R)
+    body_vels = np.array([vel_u, vel_v, vel_w])
 
+    # extract gyro rates
+    corr_gyro = u - gyro_biases
+
+    # gravity vector
+    grav_vect = np.array([0, 0, grav_acc])
+
+    ## Calculate mean f
+    # calculate inertial-to-body coordinate frame
+    R = i2bRotMatrix(x)
+    Rt = R.T
+
+    # define transform from gyro body rates to Euler rates
+    L_gyro = gyroRateTF(x)
+
+    # calculate state rate vector
+    df = np.zeros(VAR_COUNT)
+    df[VAR_ROLL:VAR_ROLL+3]     = L_gyro.dot(corr_gyro)
+    df[VAR_VEL_U:VAR_VEL_U+3]   = R.dot(grav_vect) + np.array([-drag_coeff*vel_u, -drag_coeff*vel_v, -thrust])
+    df[VAR_POS_X:VAR_POS_X+3]   = Rt.dot(body_vels)
+    
+    # integrate state rate vector to get new mean
+    f  = x + dt*df
+
+    ## Calculate Jacobians
+    # robot model Jacobian wrt x_v
+    dFx = np.zeros((VAR_COUNT, VAR_COUNT))
+    # robot model Jacobian wrt u
+    dFu = np.zeros((VAR_COUNT, 3))
+
+    # calc partial derivatives of rotation matrix
+    dR_dRoll    = i2bRotMatrixJacobianRoll(x)
+    dR_dPitch   = i2bRotMatrixJacobianPitch(x)
+    dR_dYaw     = i2bRotMatrixJacobianYaw(x)
+
+    # calc partial derivatives of gyro rate transform matrix
+    dL_dRoll    = gyroRateTFJacobianRoll(x)
+    dL_dPitch   = gyroRateTFJacobianPitch(x)
+    dL_dYaw     = gyroRateTFJacobianYaw(x)
+
+    # calc partial derivatives of inverse transform
+    dRt_dRoll   = dR_dRoll.T
+    dRt_dPitch  = dR_dRoll.T
+    dRt_dYaw    = dR_dYaw.T
+
+    # euler angular derivatives
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_ROLL] = dL_dRoll.dot(corr_gyro)
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_PITCH] = dL_dPitch.dot(corr_gyro)
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_YAW] = dL_dYaw.dot(corr_gyro)
+    
+    dFu[VAR_ROLL:VAR_ROLL+3, 0] = L_gyro.dot(np.array([1, 0, 0]))
+    dFu[VAR_ROLL:VAR_ROLL+3, 1] = L_gyro.dot(np.array([0, 1, 0]))
+    dFu[VAR_ROLL:VAR_ROLL+3, 2] = L_gyro.dot(np.array([0, 0, 1]))
+    
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_GBIAS_P] = L_gyro.dot(np.array([-1, 0, 0]))
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_GBIAS_Q] = L_gyro.dot(np.array([0, -1, 0]))
+    dFx[VAR_ROLL:VAR_ROLL+3, VAR_GBIAS_R] = L_gyro.dot(np.array([0, 0, -1]))
+
+    # body-frame translational velocity derivatives
+    dFx[VAR_VEL_U:VAR_VEL_U+3, VAR_ROLL]     = dR_dRoll.dot(grav_vect)
+    dFx[VAR_VEL_U:VAR_VEL_U+3, VAR_PITCH]    = dR_dPitch.dot(grav_vect)
+    dFx[VAR_VEL_U:VAR_VEL_U+3, VAR_YAW]      = dR_dYaw.dot(grav_vect)
+
+    dFx[VAR_VEL_U, VAR_VEL_U]    = -drag_coeff
+    dFx[VAR_VEL_U, VAR_DRAG_CO]  = -vel_u
+    dFx[VAR_VEL_V, VAR_VEL_V]    = -drag_coeff
+    dFx[VAR_VEL_V, VAR_DRAG_CO]  = -vel_v
+    dFx[VAR_VEL_W, VAR_SP_THRUST]   = -1.0
+
+    # inertial-frame position derivatives
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_ROLL]   = dRt_dRoll.dot(body_vels)
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_PITCH]  = dRt_dPitch.dot(body_vels)
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_YAW]    = dRt_dYaw.dot(body_vels)
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_VEL_U]  = Rt.dot(np.array([1, 0, 0]))
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_VEL_V]  = Rt.dot(np.array([0, 1, 0]))
+    dFx[VAR_POS_X:VAR_POS_X+3, VAR_VEL_W]  = Rt.dot(np.array([0, 0, 1]))
+
+    # integration
+    Fx = np.eye(VAR_VAR_COUNT) + dt*dFx
+    Fu = dt*dFu
+
+    ## Measurement and process noise matrices -- TUNABLE
+    M = np.diag([0.015**2, 0.015**2, 0.015**2]) * dt**2
+    
+    dR = np.zeros((VAR_VAR_COUNT, VAR_VAR_COUNT))
+    dR[VAR_ROLL, VAR_ROLL]   = 0.25**2
+    dR[VAR_PITCH, VAR_PITCH] = 0.25**2
+    dR[VAR_YAW, VAR_YAW]     = 0.25**2
+    dR[VAR_VEL_U, VAR_VEL_U] = 0.25**2
+    dR[VAR_VEL_V, VAR_VEL_V] = 0.25**2
+    dR[VAR_VEL_W, VAR_VEL_W] = 0.25**2
+    dR[VAR_SP_THRUST, VAR_SP_THRUST] = 1.5**2
+    dR[VAR_POS_X, VAR_POS_X] = 0.25**2
+    dR[VAR_POS_Y, VAR_POS_Y] = 0.25**2
+    dR[VAR_POS_Z, VAR_POS_Z] = 0.25**2
+    dR[VAR_DRAG_CO, VAR_DRAG_CO] = 1e-6**2
+    dR[VAR_OF_FL, VAR_OF_FL] = 0.000001**2
+    dR[VAR_GBIAS_P, VAR_GBIAS_P] = 1e-6**2
+    dR[VAR_GBIAS_Q, VAR_GBIAS_Q] = 1e-6**2
+    dR[VAR_GBIAS_R, VAR_GBIAS_R] = 1e-6**2
+
+    if disturb_mode == DISTURB_ACTIVE:
+      # inflate noise matrices appropriately
+      dR[VAR_ROLL:VAR_ROLL+3, VAR_ROLL:VAR_ROLL+3]      += 0.30**2*np.eye(3)
+      dR[VAR_VEL_U:VAR_VEL_U+3, VAR_VEL_U:VAR_VEL_U+3]  += 0.20**2*np.eye(3)
+      dR[VAR_POS_X:VAR_POS_X+3, VAR_POS_X:VAR_POS_X+3]  += 0.10**2*np.eye(3)
+
+    R = dR * dt**2
+
+    return (f, Fx, Fu, M, R)
 
 ########################### Observation Models
 
